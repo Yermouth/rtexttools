3e2606f4d37996e87f30317228fe68554136df6c
loren collingwood <loren.collingwood@gmail.com>
2011-02-10 22:09:12 -0800

    Added Boosting, Bagging, and Random Forest. Tim, check these out and see how well they do on your system. I was only able to partially get them to go with the memory issue here, but ran these algorithms on non-text data for classificaiton purposes and they worked well.

--- french_data_lc.R
+++ french_data_lc.R
@@ -2,7 +2,7 @@
 #R TextTools Experiments
 
 setwd("H:\\Loren")
-
+setwd("F:\\Rtexttools_group")
 #Libraries
 library(tm) # document term matrix
 library("e1071") #svm and other algorithms
@@ -10,7 +10,6 @@ library(slam) #not sure if we need this one
 library(SparseM) #Converting to Sparse Matrices
 memory.size(TRUE)
 options(stringsAsFactors=FALSE) # Be sure to set each time or at least in some of the functions
-
 
 ##############################################################
 #French Data
@@ -36,20 +35,21 @@ system.time(
     french_corp <- tm_map(french_corp, removeWords,stopwords("french"))
 )
 #Had problems here within CSDE-Terminal Server calling rJava and stuff
-#system.time(
-#    tm_map(billstrain,stemDocument)
-#)
+system.time(
+    tm_map(french_corp,stemDocument)
+)
 #Create Document Term Matrix : Can probably send to C or Python
 system.time(
     french_dtm <- DocumentTermMatrix(french_corp)
 )
+french_dtm2 <- removeSparseTerms(french_dtm,.9998) # this is really critical here to get this right
 
 #######################################################
 #           Create Train and Test Sets                #
 #######################################################
 
-train <- french_dtm[1:8000]
-test <- french_dtm[8001:10000]
+train <- french_dtm2[1:8000]
+test <- french_dtm2[8001:10000]
 
 #Convert to matrix; sparse matrix
 train3 <- as.matrix(train, "matrix.csr")
@@ -63,14 +63,12 @@ code <- as.factor(french$Codegros)[1:10000]
 train_code <- code[1:8000]
 test_code <- code[8001:10000]
 
-train_nf <- french$Codegros[1:10000]
-train_nf_code <- as.double(train_nf[1:8000])
 #####################################################
 #                 RUN MODEL -- SVM                  #
 #####################################################
 
 system.time(
-model1 <- svm(x=train3, y=train_code, method="C-classification",  cost=1000, cross=10)
+model1 <- svm(x=train3, y=train_code, method="C-classification", cross=5)
 )
 #722.16 Elapsed Seconds!!
 #1145.92 Elapsed Seconds! with 80-20 split
@@ -92,18 +90,54 @@ classAgreement(table(test_code,predtest),match.names=F) # Check accuracy, etc.
 #OK -- it worked, now just need to figure out how to improve the accuracy via tuning and such
 
 ###########################################################
-#                   Possible libraries                    #
+#                       More Learners                     #
 ###########################################################
 #Need to investigate further
 
+#Need to create dataframe for RWeka libraries
 train_data <- data.frame(cbind(train_code, train3))
 
-library(mboost) #proportional odds option
-a <- mboost(train_code~., data=train_data) #Error: evaluated nested too deeply
+library(RWeka) # Requires Java for R 32; most computers should have this
 
-library(glmnet)
-glmnet(train3, train_code, family = "multinomial") #Taking Too Damn Long
+###################################################
+#                   Boosting                      #
+###################################################
 
-#Still working on
-library(RWeka) # Need to fix java issues
-library(mvpart) #looks like takes multi-class responses
+#Boosting M1, J48 Formula
+#IMPORTANT!! NEED TO ELIMINATE ANY NON-ALPHANUMERIC COLUMNS, E.G. NO COLUMNS NAMES "..."
+#WILL PROBABLY WRITE A FUNCTION TO DEAL WITH THIS IF WE END UP GOING THE ROUTE OF 
+#MATRIX CREATION IN R
+m1 <- AdaBoostM1(as.factor(train_code)~ ., data = train_data,
+        control = Weka_control(W = "J48"))
+    
+aboost_pred <- predict(m1,data.frame(test3)) #Predict Code
+confusion <- table(test_code,aboost_pred)
+sum(diag(confusion))/length(aboost_pred) #Diagonal accuracy
+
+
+###################################################
+#                   Bagging                       #
+###################################################
+
+#Bagging = Bootstrap Averaging
+
+bagging_mod <- Bagging(as.factor(train_code)~.,data=data.frame(train_data),
+                    control=Weka_control(W="J48"))
+bagging_pred <- predict(bagging_mod,data.frame(test3))
+
+confusion <- table(test_code,bagging_pred)
+sum(diag(confusion))/length(bagging_pred) #diagonal accuracy
+
+
+###################################################
+#                  Random Forests                 #
+###################################################
+
+library(randomForest)
+
+#Trying with Congress Text
+rf_mod <- randomForest(x=train3, y=as.factor(train_code))
+rf_pred <- predict(rf_mod,newdata=test3)
+
+confusion <- table(test_code,rf_pred)
+sum(diag(confusion))/length(rf_pred) 
